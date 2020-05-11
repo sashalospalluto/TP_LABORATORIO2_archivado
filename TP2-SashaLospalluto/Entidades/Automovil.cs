26caf6b1b24d713635163be8316390e5337d3f20
Sasha <sashalospalluto@hotmail.com>
2020-05-11 03:22:47 -0300

    otra vez

---
+++ TP2-SashaLospalluto/Entidades/Automovil.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        public enum ETipo { Monovolumen, Sedan }
+
+        private ETipo tipo;
+
+        /// <summary>
+        /// Asigna los datos al automovil
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            this.tipo = ETipo.Monovolumen;
+        }
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Automovil (EMarca marca, string chasis, ConsoleColor color, ETipo tipo) :this(marca,chasis,color)
+        {
+            this.tipo = tipo;          
+        }
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return Vehiculo.ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Muestra todos los datos del Automovil
+        /// </summary>
+        /// <returns>Devuelve la cadena con todos los datos del automovil</returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine(string.Format("TAMAÑO : {0}", this.Tamanio));
+            sb.AppendLine(string.Format("TIPO : {0} ", this.tipo));
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2-SashaLospalluto/Entidades/Vehiculo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+
+        private EMarca marca;
+        private string chasis;
+        private ConsoleColor color;
+
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get;}
+
+       
+        /// <summary>
+        /// Asigna los datos pasados por parametro
+        /// </summary>
+        /// <param name="chasis"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        public Vehiculo (string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns>Devuelve la cadena de datos del vehiculo</returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        /// <summary>
+        /// Convertira a una cadena de string los datos de un vehiculo en particular
+        /// </summary>
+        /// <param name="p">Vehiculo</param>
+        public static explicit operator string (Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("CHASIS: {0}\r", p.chasis));
+            sb.Append(string.Format("MARCA : {0}\r\n", p.marca.ToString()));
+            sb.Append(string.Format("COLOR : {0}\r\n", p.color.ToString()));
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1">Vehiculo 1 </param>
+        /// <param name="v2">Vehiculo 2</param>
+        /// <returns>Devuelve true si ambos chasis son iguales, devuelve false si son diferentes </returns>
+        public static bool operator == (Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1">Vehiculo 1</param>
+        /// <param name="v2">Vehiculo 2</param>
+        /// <returns>Devuelve false si ambos chasis son iguales, devuelve true si son diferentes</returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1==v2);
+        }
+    }
+}
